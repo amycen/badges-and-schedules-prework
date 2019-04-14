2f7d769b90c2619928be784048a9d89bdc62497d
Amy Cen <amy.cen@blackrock.com>
2019-04-14 06:03:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 
 def assign_rooms(name_array)
   rooms = []
-  name_array.each_with_index! do |name, index|
+  name_array.each_with_index do |name, index|
     rooms.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
   end
   rooms
