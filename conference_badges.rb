fa25eb4bf70af9a1300d33460a56c1c94dce7c16
Amy Cen <amy.cen@blackrock.com>
2019-04-14 05:50:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,10 +5,12 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(name_array)
-  name_array.each do |name|
+  name_array.each! do |name|
     badge_maker(name)
 end
 
 def assign_rooms(name_array)
-
+  i = 1
+  name_array.each! do |name|
+    "Hello, #{name}! You'll be assigned to room #{i}!"
 end
