0fdb59f5e1d1ced5b4ac04d249e3c3f07cc53172
Amy Cen <amy.cen@blackrock.com>
2019-04-14 05:56:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,11 @@ def assign_rooms(name_array)
     "Hello, #{name}! You'll be assigned to room #{index + 1}!"
 end
 
-def printer
-  puts batch_badge_creator
+def printer(name_array)
+  batch_badge_creator(name_array).each do |badge|
+    puts badge
+  end
+  assign_rooms(name_array).each do |room|
+    puts room
+  end
 end
